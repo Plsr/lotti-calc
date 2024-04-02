8658d2a15bd4c129a4c328d72d3f78476814800d
Plsr <christianpoplawski@gmail.com>
2024-04-02 12:44:11 +0200

    Add retries on wrong answers

--- calc.py
+++ calc.py
@@ -4,8 +4,16 @@ nummer1 = random.randrange(1, 11)
 nummer2 = random.randrange(1, 11)
 
 ergebnis = input(f"{nummer1} + {nummer2} = ")
+korrekt = int(ergebnis) == nummer1 + nummer2
 
-if int(ergebnis) == nummer1 + nummer2:
+if korrekt:
   print("✅ Richtig!")
 else:
   print("❌ Falsch!")
+
+  while korrekt != True:
+    ergebnis = input(f"{nummer1} + {nummer2} = ")
+    korrekt = int(ergebnis) == nummer1 + nummer2
+    
+    if korrekt:
+      print("✅ Richtig!")
